848c456e383ea7eef155cf5a8ab42222c16744ad
apyrophob <alekshkap@icloud.com>
2020-09-22 15:53:22 +0200

    Header+NavBar+Profgile

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://wszstk.files.wordpress.com/2016/01/ogb-insider-blogs-googlelogox2-animated.gif' />
+    </header>
+};
+
+export default Header;
